644ff740f9c8e7d4a44fad87c1f754b5d073f1db
zNadir <115375151+znadir@users.noreply.github.com>
2024-09-23 11:35:29 -0400

    add circular progress bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import { View, StyleSheet } from "react-native";
 
 export default function RootLayout() {
 	return (
--- app/index.tsx
+++ app/index.tsx
@@ -2,84 +2,41 @@ import { ScrollView, StyleSheet, View } from "react-native";
 import ThemedText from "../components/ThemedText";
 import RoutineCard from "@/components/RoutineCard";
 import { setBackgroundColorAsync } from "expo-navigation-bar";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 export default function Index() {
 	setBackgroundColorAsync("black");
 
 	return (
 		<View style={styles.container}>
-			<ThemedText style={styles.title}>
-				Next Routine in <ThemedText style={styles.important}>16h 10 min</ThemedText>
-			</ThemedText>
+			{false ? (
+				<ThemedText style={styles.title}>
+					Next Routine in <ThemedText style={styles.important}>16h 10 min</ThemedText>
+				</ThemedText>
+			) : (
+				<View style={styles.header}>
+					<ThemedText style={[styles.title, styles.important]}>Déjeuner</ThemedText>
+					<ThemedText style={styles.subtitle}>13 min 10 sec left</ThemedText>
+					<View style={{ alignItems: "center" }}>
+						<CircularProgress
+							activeStrokeColor={"#ffffff"}
+							inActiveStrokeColor={"#3700FF"}
+							progressValueStyle={{ fontSize: 20, fontWeight: "400", color: "white" }}
+							value={25}
+							progressFormatter={(value: number) => {
+								"worklet";
+
+								return "10:30";
+							}}
+						/>
+					</View>
+				</View>
+			)}
 
 			<ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.routines}>
 				<RoutineCard
 					currentTaskName='École a 8h'
 					timeRemaining='18 min 10 sec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11 zsec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11 qsec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11 ssec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11 v sec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11g sec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 11x sec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 111 sec'
-					tasks={[
-						{ name: "Manger", timeRange: "8h - 8h 30" },
-						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
-					]}
-				/>
-				<RoutineCard
-					currentTaskName='École a 8h'
-					timeRemaining='18 min 12 sec'
 					tasks={[
 						{ name: "Manger", timeRange: "8h - 8h 30" },
 						{ name: "Étudier", timeRange: "8h 30 - 9h 30" },
@@ -94,15 +51,23 @@ const styles = StyleSheet.create({
 	container: {
 		paddingBottom: 70,
 	},
+	header: {
+		gap: 10,
+	},
 	title: {
 		fontSize: 25,
-		marginBottom: 30,
 		textAlign: "center",
+	},
+	subtitle: {
+		fontSize: 20,
+		textAlign: "center",
+		marginBottom: 15,
 	},
 	important: {
 		color: "#7199FF",
 	},
 	routines: {
 		gap: 13,
+		marginTop: 30,
 	},
 });
